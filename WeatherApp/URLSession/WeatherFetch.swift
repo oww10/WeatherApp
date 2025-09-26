7f31a46d280c463730aae4f38361d394a3ea52cf
oww10 <dntjr2209@gmail.com>
2025-09-26 12:42:28 +0900

    Feat: WheaterModel, Add Snapkit, Alamofire

---
+++ WeatherApp/URLSession/WeatherFetch.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherFetch.swift
+//  WeatherApp
+//
+//  Created by s s on 9/26/25.
+//
+
+import Foundation
+
+class WeatherFetch{
+    
+}
---
+++ WeatherApp/WeatherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by s s on 9/26/25.
+//
+
+import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController{
+    
+}

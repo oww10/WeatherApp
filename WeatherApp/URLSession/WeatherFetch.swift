53bb966a80424a67a95cca2c637d0bfced71d736
oww10 <dntjr2209@gmail.com>
2025-09-27 18:01:09 +0900

    ㄹ

--- WeatherApp/URLSession/WeatherFetch.swift
+++ WeatherApp/URLSession/WeatherFetch.swift
@@ -6,7 +6,21 @@
 //
 
 import Foundation
+import SnapKit
 
 class WeatherFetch{
+    
+    private let urlQueryItems: [URLQueryItem] = [
+        URLQueryItem(name: "lat", value: "37.5)"),
+        URLQueryItem(name: "lon", value: "37.5"),
+        URLQueryItem(name: "appid", value: "f57ab2ad3d1d33e78fd0c1df75b6aee0"),
+        URLQueryItem(name: "units", value: "metric")
+    ]
+    
+    private func FetchData<T: Decodable>(url: URL, completion: @escaping (T?) -> Void){
+        
+
+    }
+    
     
 }
